0d71a42080cee0756cd4dffae7fd27ebe82bfcae
Chris Vander Linden <vandch02@luther.edu>
2022-06-10 18:43:22 -0500

    updated db

--- src/index.js
+++ src/index.js
@@ -2,93 +2,87 @@ import FetchWrapper from './scripts/fetch-wrapper.js';
 import './scripts/nav-bar.js';
 
 // create new instance of FetchWrapper for getting and posting data
-const API = new FetchWrapper("https://jsonplaceholder.typicode.com/");
+const API = new FetchWrapper("https://myy-json-server.typicode.com/Chris-Vander-Linden/json-db/");
 
-// elements for book search form
+// elements for Person search form
 const resultArea = document.querySelector("#result-area ul");
-const searchBookInput = document.querySelector(`#result-area [type="search"]`);
-const searchBookForm = document.querySelector(`#result-area form`);
+const searchPersonInput = document.querySelector(`#result-area [type="search"]`);
+const searchPersonForm = document.querySelector(`#result-area form`);
 
-// elements for create book form
-const createBookTitle = document.querySelector(`#create-form-container [type="text"]`);
-const createBookBody = document.querySelector(`#create-form-container textarea`);
-const createBookForm = document.querySelector(`#create-form-container form`);
-const createBookButton = document.querySelector(`#create-form-container button`);
-const createBookResponse = document.querySelector(`#create-form-container #response`);
+// elements for create Person form
+const createPersonName = document.querySelector(`#create-form-container [type="text"]`);
+//const createPersonBody = document.querySelector(`#create-form-container textarea`);
+const createPersonForm = document.querySelector(`#create-form-container form`);
+const createPersonButton = document.querySelector(`#create-form-container button`);
+const createPersonResponse = document.querySelector(`#create-form-container #response`);
 
 /* render function used for displaying items in results section */
 const render = (search) => {
 
-  API.get("posts").then(books => {
+  API.get("test").then(data => {
     if (!search) {
       resultArea.innerHTML = "";
       // if search is blank get all results
-      books.forEach(book => {
-        resultArea.insertAdjacentHTML("beforeend", `<li><h3><i class="fa-solid fa-book"></i> ${book.title}</h3>${book.body}</li>`);
+      data.forEach(person => {
+        resultArea.insertAdjacentHTML("beforeend", `<li><i class="fa-solid ${person.icon} fa-lg"></i><div>Name: ${person.name}<br>Job: ${person.job}<br>Email: ${person.email}</div></li>`);
       });
     } else {
       resultArea.innerHTML = "";
       // if search is not blank filter results
-      books.filter(book => {
-        book.title.toUpperCase().includes(search.toUpperCase()) ? resultArea.insertAdjacentHTML("beforeend",
-          `<li><h3><i class="fa-solid fa-book"></i> ${book.title}</h3>${book.body}</li>`) : "";
+      data.filter(person => {
+        person.name.toUpperCase().includes(search.toUpperCase()) ? resultArea.insertAdjacentHTML("beforeend", `<li><i class="fa-solid ${person.icon} fa-lg"></i><div>Name: ${person.name}<br>Job: ${person.job}<br>Email: ${person.email}</div></li>`) : "";
       });
     }
   }).catch(error => {
     console.error(error);
     // if there is a server problem, write message
-    resultArea.innerHTML = "<li>We are having a problem fetching the results...</li>"
+    resultArea.innerHTML = "<li>We are having a problem fetching the results.  Please Try later.</li>"
   }).finally(() => {
     // if there are no matches display default message
     if (resultArea.innerHTML === "") {
-      resultArea.innerHTML = `<li><i class="fa-solid fa-triangle-exclamation"></i> Sorry, we couldn\'t find any books based off of the title: "${search
+      resultArea.innerHTML = `<li><i class="fa-solid fa-triangle-exclamation"></i> Sorry, we couldn\'t find any data based off of that name: "${search
         }".</li>`;
     }
   });
 }
 
-
-
-
-/* add event listeners to search book form elements */
-// get search book value after keyup
-searchBookInput.addEventListener("keyup", event => {
+// add event listeners to search Person form elements
+// get search Person value after keyup
+searchPersonInput.addEventListener("keyup", event => {
   // pass value to render function, so results can be filtered
   render(event.currentTarget.value);
 });
 
-// get search book value after search event/clicking the "x" in search field
-searchBookInput.addEventListener("search", event => {
+// get search Person value after search event/clicking the "x" in search field
+searchPersonInput.addEventListener("search", event => {
   event.preventDefault();
   // pass value to render function, so results can be filtered
   render(event.currentTarget.value);
 });
 
-searchBookForm.addEventListener("submit", event => {
+searchPersonForm.addEventListener("submit", event => {
   // prevent page refresh on submit for search form
+  // we only want to get the search value on keyup event
   event.preventDefault();
 });
 
-
-
-
-
-/* add event listener to create book form elements */
-createBookForm.addEventListener("submit", event => {
+/* add event listener to create Person form elements */
+createPersonForm.addEventListener("submit", event => {
   event.preventDefault();
   // disable submit button until after response
-  createBookButton.setAttribute("disabled", "");
-  createBookButton.textContent = "working..."
-  API.post("posts",
+  createPersonButton.setAttribute("disabled", "");
+  createPersonButton.textContent = "working..."
+  API.post("test",
     {
-      title: createBookTitle.value,
-      body: createBookBody.value
+      name: createPersonName.value,
+      //body: createPersonBody.value,
     }).then(data => {
-      createBookResponse.textContent = `The book ${data.title} has been created!`;
+      console.log(data);
+      createPersonResponse.textContent = `SUCCESS! The person with ID ${data.id} has been "fake created!"`;
     }).catch(error => console.error(error)).finally(() => {
       render();
-      createBookButton.removeAttribute("disabled");
-      createBookButton.innerHTML = `<i class="fa-solid fa-plus"></i> Create New Book`
+      createPersonButton.removeAttribute("disabled");
+      createPersonButton.innerHTML = `<i class="fa-solid fa-plus"></i> Create New Person`
     });
 });
 
